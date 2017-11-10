46d15ab3173fe2739c32248cd8a6b30361bb0a91
Ethan Czahor <ethanczahor@gmail.com>
2017-11-09 18:36:44 -0800

    bug fix for previewing home page

--- www/routes.js
+++ www/routes.js
@@ -1,6 +1,7 @@
 const nextRoutes = require('next-routes')
 const routes = module.exports = nextRoutes()
 
+routes.add('home', '/', 'index')
 routes.add('contact')
 routes.add('category', '/category/:slug/:subcategories*')
 routes.add('article', '/:category+/:idSlug')
